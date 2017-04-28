93e46527cfff79e10a6517bc76333e0fff88af6a
Jack Garcia <rexysmydog@gmail.com>
2017-04-28 14:44:33 -0500

    updating version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jack.g.garcia@hpe.com'
 license          'All rights reserved'
 description      'cookbook for installing and using hprest to configure and manage server settings'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
 supports 'redhat'
 supports 'ubuntu'
